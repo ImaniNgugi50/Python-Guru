13d84b4a194924debbb1846e013881c101b5e076
Mo. Ngugi <42456797+ImaniNgugi50@users.noreply.github.com>
2022-02-22 15:07:45 +0300

    Add files via upload

---
+++ comment.py
@@ -0,0 +1,9 @@
+#This is a comment
+print ("Hey Njeri, hope you are well.")
+
+"""
+Experimenting with multiline strings.
+This is where you have one or more sentences in your code.
+"""
+print ("Keep focusing on your goals and don't lose sight.")
+    
