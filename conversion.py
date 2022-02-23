d81c0ace0d36b1fef29deb7bf47b9911ac18d93b
Mo. Ngugi <42456797+ImaniNgugi50@users.noreply.github.com>
2022-02-23 12:41:33 +0300

    Add files via upload

---
+++ conversion.py
@@ -0,0 +1,14 @@
+#convert from float to int
+a = int(2.5)
+#convert from int to float
+b = float(5)
+#convert from int to complex
+c = complex(6)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
