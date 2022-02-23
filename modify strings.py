a1131c520a61431a333ed7bf4ca4b332aadbd94c
Mo. Ngugi <42456797+ImaniNgugi50@users.noreply.github.com>
2022-02-23 14:51:27 +0300

    Add files via upload

---
+++ modify strings.py
@@ -0,0 +1,16 @@
+d = "Hey Maureen Imani"
+print(d.upper())
+
+d = "HEY MAUREEN, I HOPE THAT YOU ARE WELL"
+print(d.lower())
+
+d = "  Hey Maureen, how is the going?"
+print(d.strip())
+
+d ="Hey Maureen, hope you are okay"
+print(d.replace("M", "L"))
+
+d ="Hey Maureen"
+print(d.split())
+
+
